3a82d74a3d4bae771359d301a559d23873631cdc
Krohnya-chan <104037493+Krohnya-chan@users.noreply.github.com>
2024-01-30 01:28:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+
+svn checkout https://github.com/openwrt/packages/trunk/libs/libnetfilter-queue package/libs/libnetfilter-queue
+git clone https://github.com/Zxilly/UA2F package/UA2F
